b78c0559dfcced8626402f8c3d58ddeb9b874dbc
168fei <95730716+168fei@users.noreply.github.com>
2022-11-24 09:13:40 +0800

    pull commit

--- src/main/java/com/airport/git/App.java
+++ src/main/java/com/airport/git/App.java
@@ -17,5 +17,6 @@ public class App
         System.out.println( "hot-fix test" );
         System.out.println( "push test" );
         System.out.println( "push test1" );
+        System.out.println( "pull test!" );
     }
 }
